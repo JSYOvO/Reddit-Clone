10bd0117b2794f00aba1729d618b1d00d91d9dc1
jaeseop yoo <wotjq5113@gmail.com>
2021-04-10 17:25:39 +0900

    Set Repository

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@
 
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
 
 const main = async () => {
