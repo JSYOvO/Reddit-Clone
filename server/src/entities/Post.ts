14e81ca96b7bf4d7e652f8784428f8debeb2c150
jaeseop yoo <wotjq5113@gmail.com>
2021-04-10 18:30:55 +0900

    Set graphQL & Set post resolver with CRUD

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,17 +1,23 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
 // import { O_NOFOLLOW } from "constants";
 
+@ObjectType() // graphQL 타입으로 변환
 @Entity()
 export class Post {
+    @Field() // graphQL 타입 스키마로 반환
     @PrimaryKey()
     id!: number;
 
+    @Field(() => String)
     @Property({ type: "date" })
     createdAt = new Date();
 
+    @Field(() => String)
     @Property({ type: "date", onUpdate: () => new Date() })
     updatedAt = new Date();
 
+    @Field()
     @Property({ type: "text" })
     title!: string;
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,13 @@
-// createdb lireddit
-
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 // import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig);
@@ -17,6 +21,30 @@ const main = async () => {
 
     // const insertedPost = await orm.em.find(Post, {});
     // console.log(insertedPost);
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false,
+        }),
+        context: () =>
+            // context이며, express의 req, res도 사용 가능
+            ({
+                em: orm.em,
+            }),
+    });
+    apolloServer.applyMiddleware({ app });
+
+    app.get("/", (_, res) => {
+        res.send("hello world");
+    });
+    app.listen(4000, () => {
+        console.log(`Server started on localhost:4000`);
+    });
 };
 
-main();
+main().catch((err) => {
+    console.error(err);
+});
