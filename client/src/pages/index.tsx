b63e6bb800bed833da95934580a1a358c59b311d
jaeseop yoo <wotjq5113@gmail.com>
2021-05-01 17:21:53 +0900

    [Front] Post 화면 구성 / [Back] Post FieldResolver 구성

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,24 +3,62 @@ import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import Layout from "../components/Layout";
 import NextLink from "next/link";
-import { Link } from "@chakra-ui/layout";
+import {
+    Box,
+    Flex,
+    Heading,
+    Link,
+    Stack,
+    Text,
+} from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
 
 const Index = () => {
-    const [{ data }] = usePostsQuery({
+    const [{ data, fetching }] = usePostsQuery({
         variables: {
             limit: 10,
         },
     });
+
+    if (!fetching && !data) {
+        return <div>You got some trouble...</div>;
+    }
+
     return (
         <Layout>
-            <NextLink href="/create-post">
-                <Link>create post</Link>
-            </NextLink>
-            {!data ? (
+            <Flex align="center" my={10}>
+                <Heading>Reddit Clone</Heading>
+                <NextLink href="/create-post">
+                    <Link ml="auto">create post</Link>
+                </NextLink>
+            </Flex>
+
+            {!data && fetching ? (
                 <div>Loading...</div>
             ) : (
-                data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+                <Stack spacing={8}>
+                    {data!.posts.map((p) => (
+                        <Box
+                            key={p.id}
+                            p={5}
+                            shadow={"md"}
+                            borderWidth={"1px"}
+                        >
+                            <Heading fontSize={"xl"}>
+                                {p.title}
+                            </Heading>
+                            <Text mt={4}>{p.textSnippet}</Text>
+                        </Box>
+                    ))}
+                </Stack>
             )}
+            {data ? (
+                <Flex>
+                    <Button isLoading={fetching} m={"auto"} my={8}>
+                        Load More
+                    </Button>
+                </Flex>
+            ) : null}
         </Layout>
     );
 };
