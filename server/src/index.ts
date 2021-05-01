52d1e5967e27cf5d14e0c78e2e38de38ff02a713
jaeseop yoo <wotjq5113@gmail.com>
2021-05-01 14:38:10 +0900

    [Backend] Mock 데이터 추가 with typeORM 마이그레이션

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,6 +13,7 @@ import { User } from "./entities/User";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import path from "path";
 
 const main = async () => {
     const conn = await createConnection({
@@ -22,8 +23,11 @@ const main = async () => {
         password: "jsyovo",
         logging: true,
         synchronize: true,
+        migrations: [path.join(__dirname, "./migrations/*")],
         entities: [Post, User],
     });
+
+    await conn.runMigrations();
 
     // await Post.delete({});
 
